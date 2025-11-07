cfc94530206cc77394ee34b154fd445f06a53d7c
Daniel González González <daniel.gonzalez@gtc.iac.es>
2025-11-07 11:24:39 +0000

    Adds general options and monitor types constants
    Introduces constants for general graphic component options and monitor types.
    
    This change centralizes these configurations, improving maintainability and providing a single source of truth for these values throughout the application.
    
    These constants define options like grid visibility, tooltip display, data handling, legend presentation, and available monitor types, making them easily accessible and modifiable.

---
+++ src/constants/generalOptions.js
@@ -0,0 +1,18 @@
+/**
+ * @returns {Object} - General options for the graphic component.
+ */
+export const GENERALOPTIONS = {
+    showGrid: true,
+    showTooltip: true,
+    gapsInData: false,
+    showLegends: true,
+    groupData: false,
+    limitMIN: "",
+    limitMAX: "",
+    animations: false,
+    microTheme: false,
+    showLegends: true,
+    legendTrunkName: false,
+    legendPosition: "Bottom",
+    scientificNotation: false,
+}
